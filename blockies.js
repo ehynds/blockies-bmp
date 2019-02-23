a2edfaa3c5d55f948e9c1dd9e394fb5b62324841
ehynds <ehynds@gmail.com>
2019-02-23 12:40:12 -0500

    fix buffer ref

--- blockies.js
+++ blockies.js
@@ -1,5 +1,5 @@
 var randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
-var Buffer = require('buffer');
+var Buffer = require('buffer').Buffer;
 
 function seedrand(seed) {
   for (var i = 0; i < randseed.length; i++) {
